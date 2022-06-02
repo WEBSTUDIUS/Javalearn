fb1548df938ab644908354493855581a03f13995
WEBSTUDIUS <advernsk@gmail.com>
2022-06-02 16:36:14 +0700

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,57 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-        static String word = "javalove";
+    static HashMap<String, String> wordsArray = new HashMap<String, String>();
+
     public static void main(String[] args) {
-        
+        wordsArray.put("javascript","A scripting or programming language that allows you to implement complex features on web pages");
+        String word = "";
+        String descr = "";
+        for(Map.Entry<String, String> item : wordsArray.entrySet()){
+
+            word = item.getKey();
+            descr = item.getValue();
+        }
         Scanner input = new Scanner(System.in);
-        System.out.println("Guess a word");
+        System.out.println("Guess a word: " + descr);
         int lenghtWord = word.length();
         String maskWord = "-".repeat(lenghtWord);
         System.out.println(maskWord);
 
         do {
-            System.out.println("Input a letter");
-            char sym = input.next().charAt(0);
-            System.out.println(maskWord);
-            if (word.indexOf(sym) >= 0) {
-                System.out.println("Yep!");
+            System.out.println("Input a letter (or word)");
+            String typedWord = input.next();
+            if (typedWord.length() > 1) {
+                if (Objects.equals(typedWord, word)) {
+                    maskWord = word;
+                } else {
+                    System.out.println("Try again");
+                    continue;
+                }
+            } else {
+                char sym = typedWord.charAt(0);
+                System.out.println(maskWord);
+                if (word.indexOf(sym) >= 0) {
+                    System.out.println("Yep!");
 //                maskWord = "";
-                for (char elem: word.toCharArray()) {
-                    if (elem == sym) {
+                    for (char elem : word.toCharArray()) {
+                        if (elem == sym) {
 //                        maskWord+=elem;
 //                    } else {
 //                        maskWord+="-";
-                        maskWord = replaceMaskedLetter(sym, maskWord);
+                            maskWord = replaceMaskedLetter(sym, maskWord, word);
+                        }
                     }
+                    System.out.println(maskWord);
+                } else {
+                    System.out.println("Try again");
                 }
-                System.out.println(maskWord);
-            } else {
-                System.out.println("Try again");
             }
         } while (maskWord.contains("-"));
         System.out.println("Congratulations! You're win!");
     }
 
-    public static String replaceMaskedLetter(char sym, String maskWord){
+    public static String replaceMaskedLetter(char sym, String maskWord, String word){
         StringBuilder newString = new StringBuilder();
         for (int i = 0; i < word.length(); i++) {
             if (word.charAt(i) == sym) {
@@ -46,4 +64,5 @@ public class Main {
         }
         return newString.toString();
     }
+
 }
